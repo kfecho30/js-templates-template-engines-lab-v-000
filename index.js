94e3d02bd4233685bf37c10842960ddca829ee5d
Kelsey Fecho <kfecho30@gmail.com>
2018-06-23 12:03:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function createPost() {
   const page = document.getElementById('page-template').value;
   const postTemplate = document.getElementById("post-template").value;
   const postTitle = document.getElementById('postTitle').value;
-  var author = document.getElementById("postAuthor").value;
+  const postBody = document.getElementById('postBody').value;
+  const postAuthor = document.getElementById("postAuthor").value;
   var postTemplate = document.getElementById("post-template").innerHTML;
   var templateFn = _.template(postTemplate);
   var postsDiv = document.getElementById("posts");
